066d3a26fdf5e4daf8389e708f5db26a06bd6528
belloibrahv <belloibrahv@gmail.com>
2025-07-16 16:26:48 +0100

    Add download_model.sh to automate model download for deployment

--- download_model.sh
+++ download_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 mkdir -p Final_predictive_model
-curl -L -o Final_predictive_model/finalized_model.sav "https://drive.google.com/uc?export=download&id=1y60p2JTTS2HGW98JrTo5Zi3MagUFXoEU"
+curl -L -o Final_predictive_model/finalized_model.sav "https://drive.google.com/file/d/1y60p2JTTS2HGW98JrTo5Zi3MagUFXoEU/view?usp=sharing"
 echo "Model downloaded successfully." 
